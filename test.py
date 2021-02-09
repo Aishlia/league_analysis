d5b5be1fa8dbdae674c054f538bd961e64c58bf3
Julia <nai.julia@gmail.com>
2021-02-08 21:56:41 -0800

    Working on ranked games only

--- test.py
+++ test.py
@@ -5,30 +5,49 @@ import random
 import requests
 from bs4 import BeautifulSoup
 import re
+import html
 
-url = 'https://na.op.gg/summoner/userName=rito+torchic'
+url = 'https://na.op.gg/summoner/matches/ajax/averageAndList/startInfo=0&summonerId=47601640&type=soloranked'
 
-r = requests.get(url).text
-soup = BeautifulSoup(r, 'html.parser')
-# mydivs = soup.findAll("div", {"class": "Team"})
-#
+# r = requests.get(url).text
+# print(r)
+
+# url = 'https://na.op.gg/summoner/userName=rito+torchic'
+
+data = requests.get(url).json()
+soup = BeautifulSoup(data['html'], 'html.parser').decode('unicode_escape')
+soup = BeautifulSoup(soup, 'html.parser')
+
+
+mydivs = soup.findAll("div", {"class": "Team"})
+
 win_loss = soup.findAll("div", {"class": "GameResult"})
 
 teams = [a.get_text() for a in soup.find_all("div", {"class": "Team"})]
 
-a = teams[0].replace('\n','|')
-a = a.split('|')
-for player in a:
-    teams[0].replace('|','')
+print(teams)
+
+for team in teams:
+    players = team.split('\n')
+    print(players)
+
+new_arr = 
 
 a = list(filter(None, a))
 
-arr = []
-
-for foo in range(0,len(a), 3):
-    new_intry = (a[foo], a[foo+2], 'https://na.op.gg/summoner/userName=' + a[foo+2].replace(' ','+'))
-    arr.append(new_intry)
-
-
-
-print(arr)
+# a = teams[0].replace('\n','|')
+# a = a.split('|')
+# for player in a:
+#     teams[0].replace('|','')
+#
+# a = list(filter(None, a))
+#
+# arr = []
+#
+# for foo in range(0,len(a), 3):
+#     new_intry = (a[foo], a[foo+2], 'https://na.op.gg/summoner/userName=' + a[foo+2].replace(' ','+'))
+#     arr.append(new_intry)
+#
+#
+#
+# print(arr)
